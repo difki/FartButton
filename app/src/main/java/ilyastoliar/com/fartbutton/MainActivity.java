b6b1e1d2de441a77e57589504f3620af43b4f09d
ilyas <ilyas@yahoo-inc.com>
2017-06-03 10:43:36 +0300

    Added new sounds and share button

--- app/src/main/java/ilyastoliar/com/fartbutton/MainActivity.java
+++ app/src/main/java/ilyastoliar/com/fartbutton/MainActivity.java
@@ -1,6 +1,7 @@
 package ilyastoliar.com.fartbutton;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ import java.util.Random;
 public class MainActivity extends Activity {
     
     ImageButton button;
+    ImageButton share;
     Random rand = new Random();
 
     @Override
@@ -21,23 +23,41 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         button = (ImageButton) findViewById(R.id.button);
+        share = (ImageButton) findViewById(R.id.share);
+
         final MediaPlayer mp1 = MediaPlayer.create(this, R.raw.sound1);
         final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.sound2);
         final MediaPlayer mp3 = MediaPlayer.create(this, R.raw.sound3);
         final MediaPlayer mp4 = MediaPlayer.create(this, R.raw.sound4);
         final MediaPlayer mp5 = MediaPlayer.create(this, R.raw.sound5);
+        final MediaPlayer mp6 = MediaPlayer.create(this, R.raw.sound6);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int n = rand.nextInt(5) + 1;
+                int n = rand.nextInt(6) + 1;
                 if (n == 1) mp1.start();
                 if (n == 2) mp2.start();
                 if (n == 3) mp3.start();
                 if (n == 4) mp4.start();
                 if (n == 5) mp5.start();
+                if (n == 6) mp6.start();
             }
 
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Download the super funny fart button https://play.google.com/store/apps/details?id=ilyastoliar.com.fartbutton&hl=en";
+                String shareSub = "Fart button";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+            }
         });
 
     }
